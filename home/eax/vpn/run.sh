0b25b42e5633b83ce74aaa4fc069d653eb3fecc6
Aleksander Alekseev <afiskon@gmail.com>
2016-10-08 12:00:04 +0300

    Minor VPN-related changes

---
+++ home/eax/vpn/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+rm /etc/resolv.conf
+systemctl stop systemd-resolved
+
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+echo 1 > /var/run/openvpn.running # for i3status
+openvpn --config client.conf || true
+rm /etc/resolv.conf
+rm /var/run/openvpn.running
+
+ln -s /var/run/systemd/resolve/resolv.conf /etc/resolv.conf
+systemctl start systemd-resolved

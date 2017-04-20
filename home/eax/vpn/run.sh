9fc708ee079a0bb1b637f7847d9cfe6295880979
Aleksander Alekseev <afiskon@gmail.com>
2017-04-20 09:12:46 +0300

    ~/vpn/run.sh updated

--- home/eax/vpn/run.sh
+++ home/eax/vpn/run.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-rm /etc/resolv.conf
+# uncomment if you are NOT using NetworkManager
 # systemctl stop systemd-resolved
+
+mv /etc/resolv.conf /etc/resolv.conf.bak
 
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 echo 1 > /var/run/openvpn.running # for i3status
 openvpn --config client.conf || true
-rm /etc/resolv.conf
 rm /var/run/openvpn.running
+mv /etc/resolv.conf.bak /etc/resolv.conf
 
-ln -s /var/run/systemd/resolve/resolv.conf /etc/resolv.conf
+# uncomment if you are NOT using NetworkManager
 # systemctl start systemd-resolved
